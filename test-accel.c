a1976b83782708f3f8798fc222423ef16e934a3f
Sean Cross <xobs@kosagi.com>
2012-06-13 17:47:33 +0800

    test-accel: Check for accelerometer thread failure
    This is the first step towards actually testing the accelerometer.

--- test-accel.c
+++ test-accel.c
@@ -7,18 +7,28 @@
 
 #define ACCEL_ADDR 0x1d
 
+#define THR_STOPPED 0
+#define THR_RUNNING 1
+#define THR_ERROR -1
+#define THR_SUCCESS 2
+
 static pthread_t i2c_thread;
 static char *i2c_return_message;
 static int i2c_return_code;
 static int should_quit;
-
+static int is_running = THR_STOPPED;
 
 void *i2c_background(void *_ignored) {
 	uint8_t start_reg = 0;
-	int fd = open("/dev/i2c-1", O_RDWR);
+	int fd;
+
+	is_running = THR_RUNNING;
+
+	fd = open("/dev/i2c-1", O_RDWR);
 	if (-1 == fd) {
 		i2c_return_code = 2;
 		i2c_return_message = "Unable to open I2C device";
+		is_running = THR_ERROR;
 		pthread_exit(NULL);
 	}
 
@@ -44,9 +54,12 @@ void *i2c_background(void *_ignored) {
 		if(ioctl(fd, I2C_RDWR, &packets) < 0) {
 			i2c_return_code = 1;
 			i2c_return_message = "I2C failed";
+			is_running = THR_ERROR;
 			pthread_exit(NULL);
 		}
 	}
+
+	is_running = THR_SUCCESS;
 	return NULL;
 }
 
@@ -56,10 +69,21 @@ int test_accel_start(void) {
 	i2c_return_message = "I2C stable";
 	should_quit = 0;
 
-	if (!pthread_create(&i2c_thread, NULL, i2c_background, NULL)) {
+	if (0 != pthread_create(&i2c_thread, NULL, i2c_background, NULL)) {
 		harness_error(0, "Unable to create I2C thread");
 		return 1;
 	}
 	harness_info(0, "Created I2C background thread");
+	return 0;
+}
+
+
+int test_accel_finish(void) {
+	if (is_running == THR_ERROR) {
+		harness_error(0, "I2C failed at some point");
+		return 1;
+	}
+	should_quit = 1;
+
 	return 0;
 }
