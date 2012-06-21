6f1b89c6bfe07c6f5d8cc9f557bdf68f9e0682c3
Sean Cross <xobs@kosagi.com>
2012-06-21 16:53:27 +0800

    test-battery: Report battery level in mV

--- test-battery.c
+++ test-battery.c
@@ -1,7 +1,18 @@
+#include <unistd.h>
 #include "fpga.h"
 #include "harness.h"
 int test_battery(void) {
-	harness_info(0, "Battery level: %d", read_battery());
+	uint32_t battery_level;
+
+	harness_info(0, "Waiting for operator to unplug AC...");
+	for (battery_level = read_battery();
+	     battery_level > 12000;
+	     battery_level = read_battery()) {
+		harness_info(0, "Battery level: %d mV", battery_level);
+		usleep(100000);
+	}
+
+	harness_info(1, "Battery level: %d mV", battery_level);
 
 	return 0;
 }
